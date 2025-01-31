d0a699642245e717183762afb8db11f67881be03
Shobhit Gupta <shobhitkumar8576@gmail.com>
2025-02-01 00:40:05 +0530

    added greet.html

--- Myproject/hello/views.py
+++ Myproject/hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, hello/index.html)
+    return render(request, "hello/index.html")
 def Shobhit(request):
     return HttpResponse("Hello, Shobhit")
 def harry(request):
@@ -11,4 +11,4 @@ def harry(request):
 def mike(request):
     return HttpResponse("Hello, Mike")
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", { "name":name.capitalize()})
