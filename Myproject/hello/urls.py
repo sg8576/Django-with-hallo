9ae773f964de954960253351cf5e40d3f9a15d0b
Shobhit Gupta <shobhitkumar8576@gmail.com>
2025-01-31 23:51:04 +0530

    Added some code

--- Myproject/hello/urls.py
+++ Myproject/hello/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("Shobhit", views.Shobhit, name="Shobhit"),
+    path("harry", views.harry, name="harry"),
+    path("mike", views.mike, name="mike")
     ]
--- Myproject/hello/views.py
+++ Myproject/hello/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    return HttpResponse("Hello World!")
+def Shobhit(request):
+    return HttpResponse("Hello, Shobhit")
+def harry(request):
+    return HttpResponse("Hello, Harry")
+def mike(request):
+    return HttpResponse("Hello, Mike")
