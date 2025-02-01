4e6fa8fca9d57a92028049e52b009fb2631bcde1
Shobhit Gupta <shobhitkumar8576@gmail.com>
2025-02-01 22:44:37 +0530

    some changes in code

--- Myproject/tasks/views.py
+++ Myproject/tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-tasks["fool","Ass", "donkey"]
+tasks=["fool","Ass", "donkey"]
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
