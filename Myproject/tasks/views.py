efcf93c709821732098ab9c5ab95eea68eba8f02
Shobhit Gupta <shobhitkumar8576@gmail.com>
2025-02-01 22:41:20 +0530

    added task file

---
+++ Myproject/tasks/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+tasks["fool","Ass", "donkey"]
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
